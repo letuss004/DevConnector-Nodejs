8cf15d5d94b1339dfff61093684431bf683a0293
letuss004 <letuss004@gmail.com>
2022-03-18 09:33:00 +0700

    webstorm push

--- config/db.js
+++ config/db.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
-const connectDB = async () => {
-	try {
-		await mongoose.connect(db);
-		console.log("MongoDB connected");
-	} catch (err) {
+const connectDB = async (uri, callback) => {
+    try {
+        await mongoose.connect(
+            db,
+            {
+                useNewUrlParser: true
+            }
+        );
+        console.log("MongoDB connected");
+    } catch (err) {
         console.error(err.message);
-        process.exit(1)
+        process.exit(1);
     }
 };
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const connectDB = require("./config/db");
 
+// define
+// todo: what express ?
 const app = express();
-
-app.get("/", (req, res) => res.send("API running"));
 const PORT = process.env.PORT || 5000;
+
+// Listen connections
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// get request
+app.get("/", (req, res) => res.send("API running"));
+
+

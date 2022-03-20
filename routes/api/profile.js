d54619bb94429e783901f5a698c5fbe4574059a8
letuss004 <letuss004@gmail.com>
2022-03-20 22:52:26 +0700

    Checkout. Get current user profile. Only haft.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-// @route   GET api/profiles
-// @desc    Test route
+// @route   GET api/profile/me
+// @desc    Get current user profile
 // @access  Public
 router.get(
     "/",
-    (req, res) => res.send("Profiles route")
+    auth,
+    async (req, res) => {
+        try {
+            const profile = await Profile.findOne(
+                {user: req.user.id}
+            ).populate(
+                'user',
+                ['name', 'avatar'],
+            );
+
+            if (!profile) {
+
+            }
+        } catch (err) {
+            console.log(err.message)
+            res.status(500).send('Server error')
+        }
+        res.send("Profiles route");
+    }
 );
 
 module.exports = router;

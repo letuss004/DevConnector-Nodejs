1e58fe68e6aa6521e88dc175e32af5d261bf22e6
magaz1nes <letuss004@gmail.com>
2022-03-20 11:24:29 +0700

    implement JWT

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const {check, validationResult, body} = require('express-validator')
 const gravatar = require("gravatar")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const User = require("../../models/User")
 
 // @route   POST api/users
@@ -55,13 +57,26 @@ router.post(
             });
             await user.save();
             // Return JWT
-            res.send("User registered");
-        } catch
-            (err) {
+            const payload = {
+                user: {
+                    id: user.id,
+                }
+            };
 
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {expiresIn: 360000},
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({token})
+                }
+            );
+        } catch (err) {
+            console.log(err.message)
+            res.status(500).send("Server error");
         }
     }
-)
-;
+);
 
 module.exports = router;

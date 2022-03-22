7921ab2fe73a8371ebfbee29a71c8287abe78603
letuss004 <letuss004@gmail.com>
2022-03-22 15:45:21 +0700

    Add post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,17 +2,44 @@ const express = require("express");
 const router = express.Router();
 const auth = require('../../middleware/auth')
 const {check, validationResult, body} = require('express-validator')
-
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+const Post = require('../../models/Post')
 
 // @route   POST api/posts
 // @desc    Create a posts
 // @access  Private
 router.post(
-    "/",
+    '/',
     auth,
-    (req, res) => {
+    check('text', 'Text is required').not().isEmpty(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400)
+                .json(
+                    {errors: errors.array()}
+                );
+        }
 
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+
+            const newPost = new Post({
+                user: req.user.id,
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+            });
+
+            const post = await newPost.save();
+            return res.json(post);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     }
 );
+
 
 module.exports = router;

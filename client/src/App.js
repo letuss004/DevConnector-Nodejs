a727da471c4466e5f02498d1fbe08c9fd4ff1ec1
letuss004 <letuss004@gmail.com>
2022-03-23 18:43:50 +0700

    Checkout, create a redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,28 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
+
 
 const App = () => (
-    <Router>
-        <Fragment>
-            <Navbar/>
-            <Routes>
-                <Route path='/' exact element={<Landing/>}/>
-            </Routes>
-            <div className="container">
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar/>
                 <Routes>
-                    <Route path='/register' exact element={<Register/>}/>
-                    <Route path='/login' exact element={<Login/>}/>
+                    <Route path='/' exact element={<Landing/>}/>
                 </Routes>
-            </div>
-        </Fragment>
-    </Router>
+                <div className="container">
+                    <Routes>
+                        <Route path='/register' exact element={<Register/>}/>
+                        <Route path='/login' exact element={<Login/>}/>
+                    </Routes>
+                </div>
+            </Fragment>
+        </Router>
+    </Provider>
 );
 
 
